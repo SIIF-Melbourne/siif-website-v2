2957a26c4b8228fec67d9621c451cf20b2644ae5
flatplum <flatplum@proton.me>
2026-02-19 17:24:03 +1100

    chore: add css imports to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  telemetry: false
+  telemetry: false,
+  css: [
+    '~/assets/css/normalize.css',
+    '~/assets/css/reusable.css',
+    '~/assets/css/global.css'
+  ],
 })
