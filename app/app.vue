9811a3a06a2b6606aaf333cccf9faacab54b3458
flatplum <flatplum@proton.me>
2026-02-19 23:49:06 +1100

    fix: ensure footer forced to bottom

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <AppHeader />
-    <NuxtPage />
-    <AppFooter />
-    <AppModal />
+  <div class="page">
+    <div class="not-footer">
+      <NuxtRouteAnnouncer />
+      <AppHeader />
+      <NuxtPage />
+    </div>
   </div>
+  <AppFooter />
+  <AppModal />
 </template>
+
+<style scoped>
+.page {
+  min-height: 100vh;
+  display: flex; 
+  flex-direction: column; 
+}
+
+.not-footer {
+  flex: 1;
+}
+</style>
