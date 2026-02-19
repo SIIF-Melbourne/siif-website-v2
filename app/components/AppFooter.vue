ce2f2e9cfe61c9e4d535d5ac567a2c158821637f
flatplum <flatplum@proton.me>
2026-02-19 18:08:33 +1100

    feat: style footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,6 +1,28 @@
 <template>
   <footer class="site-footer">
-
+    <div class="footer-left">
+      <img class="footer-left__image" src="https://lshtgdpdskhqqxdcwpjo.supabase.co/storage/v1/object/public/photos/UniMelb-Logo.svg" alt="UniMelb-Logo">
+      <div>
+        <p class="text footer-left__text"><strong>Social Impact Investment Fund</strong></p>
+        <p class="text footer-left__text">Mailbox 23, Level 4, Building 168, University of Melbourne VIC 3010</p>
+        <p class="text footer-left__text">siif.melbourne@gmail.com</p>
+        <p class="text footer-left__text">2025 Social Impact Investment Fund</p>
+      </div>
+    </div>
+    <div class="footer-right">
+      <a href="https://umsu.unimelb.edu.au/buddy-up/clubs/clubs-listing/join/7509/">
+        <img class="footer-right__icon" src="https://lshtgdpdskhqqxdcwpjo.supabase.co/storage/v1/object/public/photos/UMSU-Logo.svg" aria-hidden="true">
+      </a>
+      <a href="https://instagram.com/siifund_unimelb" aria-hidden="true">
+        <i class="footer-right__icon fab fa-instagram"></i>
+      </a>
+      <a href="https://www.facebook.com/siifund/" aria-hidden="true">
+        <i class="footer-right__icon fab fa-facebook"></i>
+      </a>
+      <a href="https://www.linkedin.com/company/siif-unimelb/?originalSubdomain=au" aria-hidden="true">
+        <i class="footer-right__icon fab fa-linkedin"></i>
+      </a>
+    </div>
   </footer>
   <noscript>
     <dialog class="nojs-popup" open>
@@ -18,9 +40,73 @@
 
 <style scoped>
 .site-footer {
-  width: 100%;
-  height: 18rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: var(--dark-blue);
+  gap: 1rem;
+  padding: 1rem;
+}
+
+.footer-left {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.footer-left__image {
+  height: 6rem;
+}
+
+.footer-left__text {
+  color: var(--off-white);
+  text-align: center;
+  font-size: 0.9rem;
+  margin: 0 0 0.2rem 0;
+}
+
+.footer-right {
+  display: flex;
+  gap: 1.5rem;
+}
+
+.footer-right__icon {
+  height: 2.5rem;
+  font-size: 2.5rem;
+}
+
+.fa-instagram {
+  background: linear-gradient(45deg, #F58529, #E4405F, #D93F8D, #8a3ab9);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+
+.fa-facebook {
+  color: #0165E1;
+}
+
+.fa-linkedin {
+  color: #0A66C2;
+}
+
+@media (min-width: 992px) {
+  .site-footer {
+    padding: 1rem 4rem;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .footer-left {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  .footer-left__text {
+    text-align: left;
+  }
 }
 
 .nojs-popup__msg-box {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   telemetry: false,
