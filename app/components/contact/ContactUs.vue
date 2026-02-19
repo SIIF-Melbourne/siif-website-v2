489a60d67e840d37f395c4c2987a5bdf78447a9f
flatplum <flatplum@proton.me>
2026-02-19 17:38:22 +1100

    feat: add email to contact

--- app/components/contact/ContactUs.vue
+++ app/components/contact/ContactUs.vue
@@ -4,7 +4,7 @@
 <template>
   <div>
     <h2 class="text text--h2">Contact us</h2>
-    <p class="text text--p">PENDING DETAIL ADDITION</p>
+    <p class="text text--p">Email us at <a class="text link" href="mailto:siif.melbourne@gmail.com">siif.melbourne@gmail.com</a>.</p>
   </div>
 </template>
 
